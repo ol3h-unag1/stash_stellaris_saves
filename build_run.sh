c727a966a4996438890611d66f9363df4e773d24
kitakaze <SuperAlphaMale2>
2024-11-27 10:48:11 +0100

    build_run.sh v1 - adding content to generated .sav files (mock)

--- build_run.sh
+++ build_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Echo the decorative line
-echo "*8*8*8*8*8*8* - - - *8*8*8*8*8*8*"
+echo "Easing in...\n† ═╬═ 7 ═╬═ 7 ═╬═ †"
 
 # Compile the C++ program
 g++ -std=c++23 -o stash stash.cpp -O3
@@ -10,7 +10,9 @@ g++ -std=c++23 -o stash stash.cpp -O3
 for i in {1..5}; do
     filename=$(printf "autosave%03d.sav" $i)
     touch "$filename"
-    echo "game_state#time_stampt" > "$filename"
+    game_state=$((i * i - 1))
+    time_stampt=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "game_state=$game_state - time_stampt=$time_stampt" > "$filename"
 done
 
 # Ask user how many user saves to create
@@ -20,7 +22,9 @@ read -p "How many user saves would you like to create? " num_user_saves
 for ((i = 1; i <= num_user_saves; i++)); do
     filename=$(printf "usersave%03d.sav" $i)
     touch "$filename"
-    echo "game_state#time_stampt" > "$filename"
+    game_state=$((i * i - 1))
+    time_stampt=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "game_state=$game_state - time_stampt=$time_stampt" > "$filename"
 done
 
 # Report on file creations
@@ -34,5 +38,4 @@ else
     echo "Error: Compiled program './stash' not found."
 fi
 
-
-echo "*9* 8**8*8*8* - - - *8*8*8*8*8*8*"
+echo "End of things...\n ⸸ ═╬═ 7 ═ ⸹⸹⸹ ╬ ⸹⸹⸹ ═ 7 ═╬═ ⸸"
