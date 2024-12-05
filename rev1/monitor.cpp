1313b4eea2638212d961d5052950017272d41d6f
kitakaze <SuperAlphaMale2>
2024-12-05 12:05:21 +0100

    Monitor init. Index modules

--- rev1/monitor.cpp
+++ rev1/monitor.cpp
@@ -8,6 +8,7 @@ namespace StashSaves::Component
 {
 
 const char* G_game_path = ".local/share/Paradox Interactive/Stellaris/save games/";
+const char* G_user_backup_path = "stash_saver/Stellaris/save games/";
 
 // will build a path where keeps its saves ;)
 auto base_path(auto&& user_name, auto&& local_game_saves_path) {
@@ -17,26 +18,55 @@ auto base_path(auto&& user_name, auto&& local_game_saves_path) {
 }
 
 v1::Monitor::Monitor() {
-
+	
+	try
+	{
 		init();
+	}
+	catch(std::exception& e)
+	{
+		std::cout << "Monitor::init() failed " << e.what() << std::endl;
+		throw;		
+	}
 }
 
 void v1::Monitor::init() {
-
-	_saves = base_path(Util::get_current_username(), G_game_path);
 
 		// get game saves directory
 			// get user directory
 		    // checko for stellaris saves dir in it
 		// create back up directory
-		// spawn index
+		// spawn index into Empire's subfolders
+
+	auto const current_user = Util::get_current_username();
+	_saves = base_path(current_user, G_game_path);
+	_backup = base_path(current_user, G_user_backup_path);
 
 	if (not Util::is_directory_exists(_saves))
 	{
-	    throw std::runtime_error("Directory does not exist: " + _saves.string());
+	    throw std::runtime_error("Game saves directory does not exist: " + _saves.string());
+	}
+
+	if (Util::is_directory_exists(_backup))
+	{
+	    std::cout << "Backup directory is ready: " << _backup << std::endl;
+	}
+	else
+	{
+		try
+		{
+			fs::create_directories(_backup);
+		}
+		catch(std::exception& e)
+		{
+			std::cout << "Failed to create backup directory: " << _backup << std::endl;
+			throw;
+		}
+
+	    std::cout << "Backup directory has been created: " << _backup << std::endl;;
 	}
 	
-    std::cout << "Monitor initialized: " << _saves << std::endl;
+    std::cout << "Monitor initialized: " << _saves << " #$$# " << _backup << std::endl;
 }
 
 void v1::Monitor::start() {
@@ -49,14 +79,22 @@ void v1::Monitor::start() {
 
 int main()
 {
+try
+{
 	using Monitor = StashSaves::Component::Monitor;
 	Monitor mon;
 
 	mon.start();
 
 	std::cout << "monitor::main()" << std::endl;
- 	std::cout << std::filesystem::current_path() << std::endl;
-
-
 	return 0;
+}
+catch (std::exception& e)
+{
+	std::cout << e.what();
+}
+catch(...)
+{
+	std::cout << "monitor.cpp::main() unknown exception" << std::endl;
+}
 }
