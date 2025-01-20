ae3ff7f9e7934932aff9955a0840b195038f2e94
013H-UNAG1 <tutnedaleko@gmail.com>
2025-01-20 17:23:38 +0000

    build and run v0

--- rev1/build_run_rev1.sh
+++ rev1/build_run_rev1.sh
@@ -1,2 +1,12 @@
-g++ -std=c++23 -Ofast -o monitor_exe monitor.cpp index.cpp util.cpp
+mkdir -p build
+cd build
+rm -f monitor_exe
+clear
+
+g++ -std=c++23 -Ofast -o monitor_exe ../monitor.cpp ../index.cpp ../util.cpp
+
 ./monitor_exe
+
+cd -
+rm -rf build
+echo "Build and Run is Done!"
