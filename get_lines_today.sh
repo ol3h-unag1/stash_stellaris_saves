a7eaa8e377096f435dc6b5517a1958fb9bd3a134
013H-UNAG1 <tutnedaleko@gmail.com>
2025-01-20 18:00:41 +0000

    Changed get_lines_today to just output todays changes

--- get_lines_today.sh
+++ get_lines_today.sh
@@ -1,15 +1,14 @@
 # Get the current date in YYYY-MM-DD format
 current_date=$(date +%F)
 
-# Get the number of lines added to the repository today
-lines_added=$(git log --since="$current_date 00:00" --until="$current_date 23:59" --pretty=tformat: --numstat | awk '{added += $1} END {print added}')
+# Get the number of lines added and removed to the repository today
+git_log_output=$(git log --since="$current_date 00:00" --until="$current_date 23:59" --pretty=tformat: --numstat)
+lines_added=$(echo "$git_log_output" | awk '{added += $1} END {print added}')
+lines_removed=$(echo "$git_log_output" | awk '{removed += $2} END {print removed}')
+lines_changed=$(echo "$git_log_output" | awk '{changed += $1 + $2} END {print changed}')
+
 clear
-echo "Number of lines pushed to the repository today: $lines_added"
-
-# Get the date for yesterday in YYYY-MM-DD format
-#yesterday_date=$(date -d "yesterday" +%F)
-
-# Get the number of lines added to the repository yesterday
-#lines_added_yesterday=$(git log --since="$yesterday_date 00:00" --until="$yesterday_date 23:59" --pretty=tformat: --numstat | awk '{added += $1} END {print added}')
-
-#echo "Number of lines pushed to the repository yesterday: $lines_added_yesterday"
+echo "Number of lines added to the repository today: $lines_added"
+echo "Number of lines removed from the repository today: $lines_removed"
+echo "Total number of lines changed in the repository today: $lines_changed"
+current_date=$(date +%F)
